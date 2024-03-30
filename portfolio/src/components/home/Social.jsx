5b74866ec7933e0eb0bff5987c1c8846334c0300
Nam Tran <you@example.com>
2024-03-30 01:46:55 -0700

    Added header and home sections

---
+++ portfolio/src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Social = () => {
+    return(
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/tr-nam/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="https://github.com/TrNam/" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+            <a href="https://www.instagram.com/itsnemhere/" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
